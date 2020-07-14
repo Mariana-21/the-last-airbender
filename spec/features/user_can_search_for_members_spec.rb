6cdbc096861d4ef95b101460b5b90aa5f6211e2d
Mariana Cid <cidmariana5@gmail.com>
2020-07-14 10:21:57 -0600

    search controller and view made

--- spec/features/user_can_search_for_members_spec.rb
+++ spec/features/user_can_search_for_members_spec.rb
@@ -10,19 +10,26 @@ describe "As a user" do
       click_on "Search For Members"
 
       expect(current_path).to eq(search_path) 
+      expect(page).to have_content("20 Members")
+      expect(page).to have_css(".member", count: 20) 
+
+      within(first(".member")) do
+        expect(page).to have_css(".name") 
+        expect(page).to have_css(".allies") 
+        expect(page).to have_css(".enemies") 
+      end
     end 
   end
 end
+# Then I should see the total number of people who live in the Fire Nation. (i.e. 20)
+# And I should see a list with the detailed information for the 20 members of the Fire Nation.
+# And for each of the members I should see:
+# - The name of the member (and their photo, if they have one)
+# - The list of allies and a list of enemies
+# - Any affiliation that the member has
 # As a user,
 # When I visit "/"
 # And I Select "Fire Nation" from the select field
 # (Note: Use the existing select field)
 # And I click "Search For Members“
 # Then I should be on page “/search”
-# Then I should see the total number of people who live in the Fire Nation. (i.e. 20)
-# And I should see a list with the detailed information for the 20 members of the Fire Nation.
-
-# And for each of the members I should see:
-# - The name of the member (and their photo, if they have one)
-# - The list of allies and a list of enemies
-# - Any affiliation that the member has
