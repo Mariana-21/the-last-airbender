372f896f84509f8e1847b4a764c3f0a187a48728
Mariana Cid <cidmariana5@gmail.com>
2020-07-14 11:03:35 -0600

    index method/ consuming an api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,13 @@
 class SearchController < ApplicationController
   def index
+    params[:nation].slice!(4)
+    tribes = params[:nation].chars
+
+    nation = tribes.join("").first(4).capitalize
+    name = tribes.join("").last(6).capitalize
+
+    conn = Faraday.new(url: "https://last-airbender-api.herokuapp.com" )
+    response = conn.get("/api/v1/characters?affiliation=#{nation+name}")
+    binding.pry
   end 
 end
