40b86ab2ced81f85dd630de6f10d762e48c6c9d5
Mariana Cid <cidmariana5@gmail.com>
2020-07-14 11:04:42 -0600

    delete pry

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,6 +8,5 @@ class SearchController < ApplicationController
 
     conn = Faraday.new(url: "https://last-airbender-api.herokuapp.com" )
     response = conn.get("/api/v1/characters?affiliation=#{nation+name}")
-    binding.pry
   end 
 end
